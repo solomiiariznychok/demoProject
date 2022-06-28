61a8a2a58de38690f1deebf58f04d323f9b760a4
solomiiariznychok <riznychoksolomiia@gmail.com>
2022-06-29 00:14:29 +0300

    refactoring

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -1,37 +1,27 @@
-import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import org.assertj.core.api.SoftAssertions;
-import org.testng.ITestResult;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pageobjects.BasePage;
 import runners.DefaultRunner;
-import utils.listeners.InvocationListener;
-import utils.listeners.TestResultsListener;
 import utils.testrail.TestRailAnnotation;
 
 import static com.codeborne.selenide.Selenide.$x;
-
+import static org.testng.Assert.assertFalse;
 
 //@Listeners(TestResultsListener.class)
-@Listeners(InvocationListener.class)
+//@Listeners(InvocationListener.class)
 public class SmokeTest extends DefaultRunner {
 
     private final String SNEAKERS_MODEL_FOR_SEARCHING = "Nike Sb Zoom Blazer Mid 864349-107";
 
     @Test
     @TestRailAnnotation.TestRail(testId = 1)
-    public void verifyMakeOrder() throws InterruptedException {
+    public void verifyMakeOrderOption() throws InterruptedException {
         new BasePage()
                 .openHomePage()
                 .selectCategories(SNEAKERS_MODEL_FOR_SEARCHING)
                 .submit();
-
-        Thread.sleep(5000);
-
+        
         var soft = new SoftAssertions();
 
         soft.assertThat($x("//h1[@class='product__title']").getValue())
@@ -54,14 +44,10 @@ public class SmokeTest extends DefaultRunner {
 
     @Test
     @TestRailAnnotation.TestRail(testId = 2)
-    @Description("Custom description")
-    public void firstTest() {
-        openPage();
-    }
-
-    @Step("Open page Allure annotation")
-    public void openPage() {
-        Selenide.open("https://rozetka.com.ua/ua/");
-        //Assert.assertFalse($x("//img[@alt='Rozetka Logo']").isDisplayed());
+    @Description("Logo is present test")
+    public void verifyLogoIsVisible() {
+        new BasePage()
+                .openHomePage();
+        assertFalse($x("//img[@alt='Rozetka Logo']").isDisplayed());
     }
 }
